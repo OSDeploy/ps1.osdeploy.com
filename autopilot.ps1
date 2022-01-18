4fe2fe7d59adecd9e7fcb39948da6760c8bc194d
David Segura <david@segura.org>
2022-01-18 15:54:56 -0600

    22.1.18

--- autopilot.ps1
+++ autopilot.ps1
@@ -28,16 +28,6 @@ if ((Get-ExecutionPolicy) -ne 'RemoteSigned')
     Set-ExecutionPolicy RemoteSigned -Force
 }
 #=================================================
-#	PackageManagement
-#=================================================
-if (Get-Module -Name PackageManagement -ListAvailable | Where-Object {$_.Version -ge '1.4.7'}) {
-    Write-Host -ForegroundColor Cyan 'PackageManagement 1.4.7 or greater is installed'
-}
-else {
-    Write-Host -ForegroundColor Cyan 'Install-Package PackageManagement'
-    Install-Package -Name PackageManagement -MinimumVersion 1.4.7 -Force -Confirm:$false -Source PSGallery
-}
-#=================================================
 #	PowerShellGet
 #=================================================
 if (Get-Module -Name PowerShellGet -ListAvailable | Where-Object {$_.Version -ge '2.2.5'}) {
@@ -46,16 +36,7 @@ if (Get-Module -Name PowerShellGet -ListAvailable | Where-Object {$_.Version -ge
 else {
     Write-Host -ForegroundColor Cyan 'Install-Package PowerShellGet'
     Install-Package -Name PowerShellGet -MinimumVersion 2.2.5 -Force -Confirm:$false -Source PSGallery
-}
-#=================================================
-#	NuGet
-#=================================================
-Import-Module PackageManagement,PowerShellGet -Force
-$PackageProvider = Get-PackageProvider -Name NuGet -ErrorAction Ignore
-if (-not ($PackageProvider))
-{
-    Write-Host -ForegroundColor Cyan 'Install-PackageProvider NuGet'
-    Install-PackageProvider -Name NuGet -ForceBootstrap -IncludeDependencies
+    Import-Module PowerShellGet,PackageManagement -Force
 }
 #=================================================
 #	Trust PSGallery
