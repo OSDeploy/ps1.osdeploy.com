16537d0f37fb2049c80979b233851825520a7888
David Segura <david@segura.org>
2022-01-18 21:15:34 -0600

    22.1.18

--- autopilot.ps1
+++ autopilot.ps1
@@ -1,16 +1,19 @@
-'Autopilot Requirements PSCloudScript'
-'autopilot.ps1.osdeploy.com'
-
-'Requirements:'
-'Windows 10 1809+ - Windows 11'
-
-'Environment:'
-'OOBE (Out-of-Box Experience)'
-'Press Shift + F10 to open a Command Prompt'
-
-'Command Line (Command Prompt)'
-'powershell iex(irm autopilot.ps1.osdeploy.com)'
-
+<#
+.SYNOPSIS
+    Autopilot PSCloudScript at autopilot.ps1.osdeploy.com
+.DESCRIPTION
+    A detailed description of the function or script. This keyword can be
+    used only once in each topic.
+.NOTES
+    Author          : David Segura
+    Operating System: Windows 10 1809+ - Windows 11
+    Environment     : OOBE (Out-of-Box Experience)
+                    : Press Shift + F10 to open a Command Prompt
+.LINK
+    autopilot.ps1.osdeploy.com
+.EXAMPLE
+    powershell iex(irm autopilot.ps1.osdeploy.com)
+#>
 #=================================================
 #   Block WinPE
 #=================================================
@@ -38,17 +41,17 @@ if ((Get-ExecutionPolicy) -ne 'RemoteSigned')
     Set-ExecutionPolicy RemoteSigned -Force
 }
 #=================================================
-#	PowerShellGet,PackageManagement
+#	PackageManagement,PowerShellGet
 #=================================================
 if (Get-Module -Name PowerShellGet -ListAvailable | Where-Object {$_.Version -ge '2.2.5'}) {
     Write-Host -ForegroundColor Cyan 'PowerShellGet 2.2.5 or greater is installed'
 }
 else {
-    Write-Host -ForegroundColor Cyan 'Install-Package PowerShellGet,PackageManagement'
+    Write-Host -ForegroundColor Cyan 'Install-Package PackageManagement,PowerShellGet'
     Install-Package -Name PowerShellGet -MinimumVersion 2.2.5 -Force -Confirm:$false -Source PSGallery
 
-    Write-Host -ForegroundColor Cyan 'Import-Module PowerShellGet,PackageManagement'
-    Import-Module PowerShellGet,PackageManagement -Force
+    Write-Host -ForegroundColor Cyan 'Import-Module PackageManagement,PowerShellGet'
+    Import-Module PackageManagement,PowerShellGet -Force
 }
 #=================================================
 #	Set-PSRepository PSGallery
@@ -65,17 +68,17 @@ if ($PSRepository)
 #=================================================
 #	WindowsAutopilotIntune
 #=================================================
-$Module = Import-Module WindowsAutopilotIntune -PassThru -ErrorAction Ignore
-if (-not $Module)
+$Requirement = Import-Module WindowsAutopilotIntune -PassThru -ErrorAction Ignore
+if (-not $Requirement)
 {
-    Write-Host -ForegroundColor Cyan 'Install-Module WindowsAutopilotIntune'
-    Install-Module WindowsAutopilotIntune -Force -Verbose
+    Write-Host -ForegroundColor Cyan 'Install-Module AzureAD,Microsoft.Graph.Intune,WindowsAutopilotIntune'
+    Install-Module WindowsAutopilotIntune -Force
 }
 #=================================================
 #	AzureAD
 #=================================================
-$Module = Import-Module AzureAD -PassThru -ErrorAction Ignore
-if (-not $Module)
+$Requirement = Import-Module AzureAD -PassThru -ErrorAction Ignore
+if (-not $Requirement)
 {
     Write-Host -ForegroundColor Cyan 'Install-Module AzureAD'
     Install-Module AzureAD -Force -Verbose
@@ -83,12 +86,15 @@ if (-not $Module)
 #=================================================
 #	Get-WindowsAutoPilotInfo
 #=================================================
-Write-Host -ForegroundColor Cyan 'Install-Script Get-WindowsAutoPilotInfo'
-
-#Install-Script -Name Get-WindowsAutoPilotInfo -Force -Verbose
+$Requirement = Get-InstalledScript -Name Get-WindowsAutoPilotInfod -ErrorAction SilentlyContinue
+if (-not $Requirement)
+{
+    Write-Host -ForegroundColor Cyan 'Install-Script Get-WindowsAutoPilotInfo'
+    Install-Script -Name Get-WindowsAutoPilotInfo -Force
+}
 #=================================================
 #	Complete
 #=================================================
-Write-Verbose 'Get-WindowsAutoPilotInfo is ready to run in a new PowerShell window' -Verbose
-Start-Sleep -Seconds 5
+Write-Verbose 'Starting a new PowerShell process for Get-WindowsAutoPilotInfo' -Verbose
+Start-Sleep -Seconds 3
 Start-Process PowerShell.exe
